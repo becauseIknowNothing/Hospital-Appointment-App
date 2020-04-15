46afa6fa49b146bfabd6f7438c329383fd5c7f75
Lovish <lovish.madhu.2404@gmail.com>
2020-04-15 20:19:19 +0100

    Improvements

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
 module.exports={
-    URL:"mongodb+srv://lovish:lovish@cluster0-9i4rb.mongodb.net/test?retryWrites=true&w=majority"
+    URL:"addyourclusterurlhere"
     ,
     secret:"JWT_SECRET=addjsonwebtokensecretherelikeQuiscustodietipsoscustodes"
     ,
